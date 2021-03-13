00d531ee7898a0203a5c6bb3d308f65b05087ad0
Myungseo Kang <l3opold7@gmail.com>
2021-03-14 03:28:43 +0900

    Update 42748.py

--- sorting/42748.py
+++ sorting/42748.py
@@ -3,7 +3,7 @@ def solution(array, commands):
     for command in commands:
         i, j, k = command
         if i == j:
-            answer.append(array[i - 1:][k - 1])
+            answer.append(array[i - 1])
         else:
             sorted_data = sorted(array[i - 1:j])
             answer.append(sorted_data[k - 1])
